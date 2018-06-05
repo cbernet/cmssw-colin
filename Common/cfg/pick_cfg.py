045aee881cc4bb4c214781f41a2b635af4cd0717
Colin <colin.bernet@cern.ch>
2018-06-05 10:23:00 +0200

    added merge and pick

---
+++ Common/cfg/pick_cfg.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+##____________________________________________________________________________||
+process = cms.Process("MERGE")
+
+##____________________________________________________________________________||
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+list_of_files = []
+
+##____________________________________________________________________________||
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(
+        list_of_files
+        )
+    eventsToProcess = cms.untracked.VEventRange('1:1-1:10'),
+    )
+
+##____________________________________________________________________________||
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string('merge.root'),
+    SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
+    outputCommands = cms.untracked.vstring(
+        'keep *'
+        )
+    )
+
+##____________________________________________________________________________||
+process.options   = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.MessageLogger.cerr.FwkReport.reportEvery = 50
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+
+##____________________________________________________________________________||
+process.p = cms.Path()
+
+process.e1 = cms.EndPath(
+    process.out
+    )
+
+##____________________________________________________________________________||
+
